ef308909988061045d8a1be3f3ac954c657b3170
Matteo Buttazzi <mbuttazzi@lagrev1.ee.ethz.ch>
2023-03-29 16:10:06 +0200

    modifiche per cf

--- peripherals/uart/test.c
+++ peripherals/uart/test.c
@@ -24,6 +24,9 @@
 #define BUFFER_SIZE 16
 #define UART_BAUDRATE 115200
 
+int tx_buffer[BUFFER_SIZE];
+int rx_buffer[BUFFER_SIZE];
+
 int uart_read_nb(int uart_id, void *buffer, uint32_t size)
 {
   int periph_id = ARCHI_UDMA_UART_ID(uart_id);
@@ -56,8 +59,26 @@ int main()
 {
 
 int error = 0;
-int tx_buffer[BUFFER_SIZE] = {'S','t','a','y',' ','a','t',' ','h','o','m','e','!','!','!','!'};
-int rx_buffer[BUFFER_SIZE];
+int setup;
+int k;
+int a;
+
+tx_buffer[0] = 'S';
+tx_buffer[1] =  't';
+tx_buffer[2] =  'a';
+tx_buffer[3] =  'y';
+tx_buffer[4] =  ' ';
+tx_buffer[5] =  'a';
+tx_buffer[6] =  't';
+tx_buffer[7] =  ' ';
+tx_buffer[8] = 'h';
+tx_buffer[9] = 'o';
+tx_buffer[10] = 'm';
+tx_buffer[11] = 'e';
+tx_buffer[12] = '!';
+tx_buffer[13] = '!';
+tx_buffer[14] = '!';
+tx_buffer[15] = '!';
 
 for (int u = 0; u < ARCHI_UDMA_NB_UART; ++u)
 {
@@ -69,19 +90,65 @@ for (int u = 0; u < ARCHI_UDMA_NB_UART; ++u)
   printf("[%d, %d] Start test uart %d\n",  get_cluster_id(), get_core_id(), u);
   uart_open(u,UART_BAUDRATE);
 
-  for (int i = 0; i < BUFFER_SIZE; ++i)
+  k=0;
+  a=0;
+  //configure pin function for RTS e CTS
+  setup = pulp_read32(0x1A104018);
+  setup = setup | 0x00000028;
+  pulp_write32(0x1A104018, setup);
+  //enable control flow inside UART peripheral
+  setup = pulp_read32(ARCHI_UDMA_ADDR + UDMA_UART_OFFSET(channel) + UDMA_CHANNEL_CUSTOM_OFFSET + UART_SETUP_OFFSET);
+  setup = setup | 0x00C0;
+  pulp_write32(ARCHI_UDMA_ADDR + UDMA_UART_OFFSET(channel) + UDMA_CHANNEL_CUSTOM_OFFSET + UART_SETUP_OFFSET, setup);
+  //fase 1 e 2
+  while(a < BUFFER_SIZE)
   {
-    uart_write_nb(u,tx_buffer + i,1); //--- non blocking write
-    uart_read(u,rx_buffer + i,1);     //--- blocking read
-    
-    if (tx_buffer[i] == rx_buffer[i])
+    if(pulp_read32(ARCHI_UDMA_ADDR + UDMA_UART_OFFSET(channel) + UDMA_CHANNEL_CUSTOM_OFFSET + UART_STATUS_OFFSET) & 0x0004)
     {
-      printf("PASS: tx %c, rx %c\n", tx_buffer[i],rx_buffer[i]);
+      uart_read(u,rx_buffer + k,1);     //--- blocking read
+      k=k+1;
+      //uart_write(u,tx_buffer + a,1);  //--- blocking write
+      //a=a+1;
+    }
+    else
+    {
+      uart_write(u,tx_buffer + a,1);  //--- blocking write
+      a=a+1;
+    }
+  }
+  //fase 3
+  while(k < BUFFER_SIZE)
+  {
+    uart_read(u,rx_buffer + k,1);     //--- blocking read
+    k=k+1;
+  }
+
+  for(int b = 0; b < BUFFER_SIZE; ++b)
+  {    
+    //uart_write_nb(u,tx_buffer + b,1); //--- non blocking write
+    //uart_read(u,rx_buffer + b,1);     //--- blocking read
+    if (tx_buffer[b] == rx_buffer[b])
+    {
+      printf("PASS: tx %c, rx %c\n", tx_buffer[b],rx_buffer[b]);
     }else{
-      printf("FAIL: tx %c, rx %c\n", tx_buffer[i],rx_buffer[i]);
+      printf("FAIL: tx %c, rx %c\n", tx_buffer[b],rx_buffer[b]);
       error++;
     }
   }
 }
   return error;
 }
+
+/*for (int i = 0; i < BUFFER_SIZE; ++i)
+  {
+    uart_write(u,tx_buffer + i,1); //--- non blocking write
+    uart_read(u,rx_buffer + i,1);     //--- blocking read
+    if(i > 10)
+    {
+      if(a < BUFFER_SIZE)
+      {
+        uart_read(u,rx_buffer + a,1);     //--- blocking read
+        a++;
+      }
+    }
+  }*/
